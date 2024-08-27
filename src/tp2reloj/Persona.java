d5788ab128a872e71fb4c87efc68d27b52135f89
lucianoYB <lucianocamano8@gmail.com>
2024-08-27 19:15:58 -0300

    añadi varianles , miren si esta todo bien

--- src/tp2reloj/Persona.java
+++ src/tp2reloj/Persona.java
@@ -7,7 +7,64 @@ package tp2reloj;
 
 public class Persona {
   
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private double altura;
+    private Reloj reloj;
+
+    public Persona(String nombre, String apellido, int edad, double altura, Reloj reloj) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.altura = altura;
+        this.reloj = reloj;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+    
+    public void decirHora(){
+        System.out.println("La hora es : " +reloj.getHora());
+    }
+     
+    public void hablar(){
         
+    }
+    
+    public void comer (){
+        
+    }
+      
     }
     
     
