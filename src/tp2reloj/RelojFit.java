2292987d7705f9c6903ff1b0f502ee0cebb2bb6e
lucianoYB <lucianocamano8@gmail.com>
2024-08-27 19:16:39 -0300

    añadi variables , miren si esta todo bien

--- src/tp2reloj/RelojFit.java
+++ src/tp2reloj/RelojFit.java
@@ -4,10 +4,36 @@
  */
 package tp2reloj;
 
-/**
- *
- * @author Equipo
- */
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+
 public class RelojFit extends Reloj{
+
+    public RelojFit(LocalDate dia, LocalTime hora, String modelo, long numSerie) {
+        super(dia, hora, modelo, numSerie);
+    }
+
+    public RelojFit() {
+    }
     
+    public void cuentaPasos ( int xFinal , int yFinal){
+        
+        int xInicial=0;
+        int yInicial=0;
+        
+        int d1 = (xFinal - xInicial) * (xFinal  - xInicial );
+        int d2 = (yFinal - yInicial ) * (yFinal - yInicial );
+        
+        double distancia = Math.sqrt( d1 + d2);
+        
+        int pasos = (int) Math.round(distancia);
+        
+        System.out.println("Pasos:  " + pasos);
+              
+    }
+    
+    public void frecuenciaAleatoria(){
+        
+    }
 }
